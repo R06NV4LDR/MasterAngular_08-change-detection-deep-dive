e2d43339fcfd0a9db852d46a28e656b8af8bb36b
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-03 13:45:21 +0200

    reinstated the signals all over the app | removed zone.js from angular.json:build

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,26 +1,17 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  DestroyRef,
-  inject,
-  OnInit,
-} from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { MessagesService } from "../messages.service";
-import { AsyncPipe } from "@angular/common";
+
 
 @Component({
   selector: "app-messages-list",
   standalone: true,
-  imports: [AsyncPipe],
   templateUrl: "./messages-list.component.html",
   styleUrl: "./messages-list.component.css",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  messages$ = this.messagesService.messages$;
-
+  messages = this.messagesService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
